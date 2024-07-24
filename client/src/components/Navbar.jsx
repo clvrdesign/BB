130313f556ddd3e4ec757a47a51d0fc0e59a020a
clvrdesign628 <clvrdesign628@gmail.com>
2024-07-24 17:27:18 +0200

    Update the homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -75,7 +75,7 @@ function Navbar() {
 
           <div ref={iconRef}  onClick={toggleProfileMenu} className="relative w-10 h-10 cursor-pointer flex justify-center items-center text-slate-50 bg-slate-400 rounded-full z-50">
             <div className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-full overflow-hidden">
-                <img className='w-full h-full object-cover' src={assets.profile} alt="" />
+                <img className='w-full h-full object-cover' src={assets.imgMale} alt="" />
             </div>
 
             {profileMenu && (
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import Slider from '../../components/Slider'
 import Post from '../../components/Post'
@@ -8,9 +9,19 @@ function Home() {
       <Navbar />
       <div className='container sm:mx-auto sm:my-5 m-auto lg:h-48 h-36 bg-slate-100 overflow-hidden sm:rounded-3xl'>
         <Slider/>
-        <div className="flex h-80">
+      </div>
+      <div className="container sm:mx-auto sm:my-5 m-auto lg:h-48 h-36 lg:bg-slate-100 overflow-hidden sm:rounded-3xl">
+        <ul className="lg:flex lg:flex-col hidden bg-slate-100">
+            <li><Link className='p-2' to='/'>Fondateur</Link></li>
+            <li><Link to='/'>Vision</Link></li>
+            <li><Link to='/'>Calendar</Link></li>
+            <li><Link to='/'>Media</Link></li>
+            <li><Link to='/'>Publications</Link></li>
+            <li><Link to='/'>Contact-nous</Link></li>
+        </ul>
+        <div className="flex flex-col-reverse">
           <Post/>
-          Hello dolly
+          
         </div>
       </div>
     </>
