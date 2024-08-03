f1ca1ad7386a5a7950d07c9dd5b9209d80f5c4d6
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-03 06:44:56 +0200

    "Added Post and Profile routes to App.jsx, and updated Post.jsx to include links to Profile and Post pages."

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import Post from './pages/post/Post';
+import Profile from './pages/profile/Profile';
 import ErrorPage from './pages/error/Error404';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
