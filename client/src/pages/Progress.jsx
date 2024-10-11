7b59557e9d3aaf901220eab75f919c445ad72bd9
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-10-11 08:21:16 +0200

    Another commit

--- client/src/pages/Progress.jsx
+++ client/src/pages/Progress.jsx
@@ -6,7 +6,7 @@ const Progress = () => {
         <div className="w-full h-screen flex flex-col items-center justify-center px-3 bg-slate-100">
             <div className="md:w-[375px] w-full bg-white flex flex-col gap-4 items-center justify-center py-10 px-10 rounded-xl">
                 <p className="text-sm text-center text-slate-600">
-                    Working on the registration form that can be tough, Please consider coming back later...
+                    Working on the registration form can be tough. Please consider coming back later.
                 </p>
                 <Button className="bg-slate-800">
                     <Link to='/'>Back home</Link>
