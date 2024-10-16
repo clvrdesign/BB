f5db6650497f38dfd51163c55c6a7092f08d7ca0
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-10-16 13:31:53 +0200

    Added some adjustments

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Search = ({children}) => {
   return (
-    <div className="fixed top-16 left-0 w-full bg-white py-4 px-3 z-50">
+    <div className="fixed top-16 left-0 w-full bg-white drop-shadow-sm py-4 px-3 z-50">
         {children}
     </div>
   )
