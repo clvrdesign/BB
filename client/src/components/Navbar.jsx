3490f6b837010e480213d3005e73750a88cd4361
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-10-21 08:06:36 +0200

    "Removed form validation and handling logic, updated form fields and layout, and added new fields and checkboxes in the Add Member page."

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -139,6 +139,14 @@ function Navbar({ onClick }) {
                     <li>
                       <Link
                         className="flex gap-2 py-2 px-5 ease-out delay-75 hover:bg-slate-200 hover:text-primary-accent rounded-full"
+                        to="/member/register"
+                      >
+                        <i className="bi bi-people"></i> Devenir membre actif
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="flex gap-2 py-2 px-5 ease-out delay-75 hover:bg-slate-200 hover:text-primary-accent rounded-full"
                         to="/"
                       >
                         <i className="bi bi-person-plus"></i> Ajouter membre
@@ -196,6 +204,14 @@ function Navbar({ onClick }) {
                     <Link
                       className="flex gap-2 py-2 px-5 ease-out delay-75 hover:bg-slate-100 hover:text-primary-accent rounded-full"
                       to="/member/addMember"
+                    >
+                      <i className="fi fi-rr-users translate-y-0.5"></i> Membre actif
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="flex gap-2 py-2 px-5 ease-out delay-75 hover:bg-slate-100 hover:text-primary-accent rounded-full"
+                      to="/"
                     >
                       <i className="fi fi-rr-user-add translate-y-0.5"></i> Ajouter membre
                     </Link>
--- client/src/pages/member/AddMember.jsx
+++ client/src/pages/member/AddMember.jsx
@@ -1,76 +1,50 @@
-import { useEffect, useState, useRef } from 'react';
+// import { useState } from 'react';
 import Button from "../../components/Button";
 import { Link } from 'react-router-dom'
 
 const AddMem = () => {
-  const dayRef = useRef(null);
-  const [isFebruary, setIsFebruary] = useState(false);
-  const [formValues, setFormValues] = useState({
-    lastName: '',
-    firstName: '',
-    email: '',
-    phoneSMS: '',
-    phoneWhatsApp: '',
-    year: ''
-  });
-  const [errors, setErrors] = useState({});
+  // const [formValues, setFormValues] = useState({
+  //   lastName: '',
+  //   firstName: '',
+  //   email: '',
+  //   phoneSMS: '',
+  //   phoneWhatsApp: '',
+  //   year: ''
+  // });
+  // const [errors, setErrors] = useState({});
 
-  const checkMonth = () => {
-    if (dayRef.current && dayRef.current.value === '2') {
-      setIsFebruary(true);
-    } else {
-      setIsFebruary(false);
-    }
-  };
 
-  useEffect(() => {
-    const handleChange = () => {
-      checkMonth();
-    };
+  // const validateForm = () => {
+  //   const newErrors = {};
+  //   if (!formValues.lastName) newErrors.lastName = 'Le nom est requis';
+  //   if (!formValues.firstName) newErrors.firstName = 'Le prénom est requis';
+  //   if (!formValues.email) newErrors.email = 'L\'Email est requis';
+  //   if (!/\S+@\S+\.\S+/.test(formValues.email)) newErrors.email = 'L\'email est invalide';
+  //   if (!formValues.phoneSMS) newErrors.phoneSMS = 'Le numéro de téléphone est requis';
+  //   if (!/^[0-9]{8}$/.test(formValues.phoneSMS)) newErrors.phoneSMS = 'Phone number must be 8 digits';
+  //   if (!formValues.year) newErrors.year = 'Year of birth is required';
+  //   return newErrors;
+  // };
 
-    const daySelect = dayRef.current;
-    if (daySelect) {
-      daySelect.addEventListener('change', handleChange);
-    }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const validationErrors = validateForm();
+  //   if (Object.keys(validationErrors).length === 0) {
+  //     console.log('Form submitted', formValues);
+  //   } else {
+  //     setErrors(validationErrors);
+  //   }
+  // };
 
-    return () => {
-      if (daySelect) {
-        daySelect.removeEventListener('change', handleChange);
-      }
-    };
-  }, []);
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormValues({
+  //     ...formValues,
+  //     [name]: value
+  //   });
+  // };
 
-  const validateForm = () => {
-    const newErrors = {};
-    if (!formValues.lastName) newErrors.lastName = 'Le nom est requis';
-    if (!formValues.firstName) newErrors.firstName = 'Le prénom est requis';
-    if (!formValues.email) newErrors.email = 'L\'Email est requis';
-    if (!/\S+@\S+\.\S+/.test(formValues.email)) newErrors.email = 'L\'email est invalide';
-    if (!formValues.phoneSMS) newErrors.phoneSMS = 'Le numéro de téléphone est requis';
-    if (!/^[0-9]{8}$/.test(formValues.phoneSMS)) newErrors.phoneSMS = 'Phone number must be 8 digits';
-    if (!formValues.year) newErrors.year = 'Year of birth is required';
-    return newErrors;
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const validationErrors = validateForm();
-    if (Object.keys(validationErrors).length === 0) {
-      console.log('Form submitted', formValues);
-    } else {
-      setErrors(validationErrors);
-    }
-  };
-
-  const handleInputChange = () => {
-    // const { name, value } = e.target;
-    // setFormValues({
-    //   ...formValues,
-    //   [name]: value
-    // });
-  };
-
-  handleInputChange()
+  // handleInputChange()
 
   return (
     <div className="w-full min-h-screen bg-slate-100 md:py-5 py-0 md:px-3 px-0 flex md:flex-row flex-col items-center justify-center">
@@ -81,8 +55,8 @@ const AddMem = () => {
               <Link to='/' className='pl-1 text-primary-accent'>Accueil</Link>
             </small>
           </div>
-          <h1 className='text-[2rem] font-bold text-gray-600 mb-4'>Inscription Membre Actif</h1>
-          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-3">
+          <h1 className='text-[2rem] font-bold text-gray-600 mb-4'>Devenir Membre Actif</h1>
+          <form onSubmit='' className="w-full flex flex-col gap-3">
 
             <fieldset>
               <legend className="text-lg font-bold text-gray-600 mb-2">1. Type de volontariat</legend>
@@ -90,32 +64,32 @@ const AddMem = () => {
               <div className="grid sm:grid-cols-2 grid-cols-1">
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Enregistrat membre</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Enregistrat membre</label>
                 </div>
 
-                <div className='flex gap-1'>
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Administration</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Administration</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Communication</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Communication</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Informatique</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Informatique</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Gestion et logistique</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Gestion et logistique</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Compagne et mobilisation</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Compagne et mobilisation</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Mandataire politique</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Mandataire politique</label>
                 </div>
               </div>
 
@@ -124,27 +98,27 @@ const AddMem = () => {
             <fieldset>
               <legend className="text-lg font-bold text-gray-600 mb-2">2. Candidats aux éléctions</legend>
 
-              <div className="grid sm:grid-cols-2 grid-cols-1">
-                <div className='flex gap-1'>
+              <div className="grid sm:grid-cols-2 grid-cols-1 gap-2">
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Candidat Député</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Candidat Député</label>
                 </div>
 
-                <div className='flex gap-1'>
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Candidat conseil communal</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Candidat conseil communal</label>
                 </div>
-                <div className='flex gap-1'>
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Candidat Sénataire</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Candidat Sénataire</label>
                 </div>
-                <div className='flex gap-1'>
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Candidat Conseil collinaire</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Candidat Conseil collinaire</label>
                 </div>
-                <div className='flex gap-1'>
+                <div className='flex items-center gap-1'>
                   <input type="checkbox" name="" id="add" />
-                  <label htmlFor="add">Candidat chef de quartier</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Candidat chef de quartier</label>
                 </div>
 
               </div>
@@ -159,13 +133,13 @@ const AddMem = () => {
                   type="text"
                   id='confirmPassword'
                   placeholder="Mukaza, Bujumbura"
-                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none ${errors.lastName ? 'border-red-500' : ''}`}
+                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none `}
                 />
                 <input
                   type="text"
                   id='confirmPassword'
                   placeholder="Mukaza, Bujumbura"
-                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none ${errors.lastName ? 'border-red-500' : ''}`}
+                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none `}
                 />
 
               </div>
@@ -180,13 +154,13 @@ const AddMem = () => {
                   type="text"
                   id='confirmPassword'
                   placeholder="Mukaza, Bujumbura"
-                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none ${errors.lastName ? 'border-red-500' : ''}`}
+                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none `}
                 />
                 <input
                   type="text"
                   id='confirmPassword'
                   placeholder="Mukaza, Bujumbura"
-                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none ${errors.lastName ? 'border-red-500' : ''}`}
+                  className={`h-10 bg-gray-100 text-sm px-4 rounded-md focus:border-primary-color border outline-none `}
                 />
 
               </div>
@@ -195,7 +169,7 @@ const AddMem = () => {
 
             <fieldset className='w-full'>
               <legend className="text-lg font-bold text-gray-600 mb-2">5.Expérience professionelle</legend>
-              <textarea className='w-full h-40 bg-gray-100 text-sm p-4 rounded-md focus:border-primary-color border outline-none' name="" id=""></textarea>
+              <textarea className='w-full h-40 bg-gray-100 text-sm px-4 py-3 rounded-md focus:border-primary-color border outline-none' name="" id=""></textarea>
 
 
             </fieldset>
@@ -206,55 +180,55 @@ const AddMem = () => {
               <div className="grid sm:grid-cols-3 grid-cols-1">
                 <div className='flex gap-1'>
                   <input type="radio" name="ethnic" id="add" />
-                  <label htmlFor="add">Umuhutu</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Umuhutu</label>
                 </div>
 
                 <div className='flex gap-1'>
                   <input type="radio" name="ethnic" id="add" />
-                  <label htmlFor="add">Umututsi</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Umututsi</label>
                 </div>
                 <div className='flex gap-1'>
                   <input type="radio" name="ethnic" id="add" />
-                  <label htmlFor="add">Umutwa</label>
+                  <label htmlFor="add" className='text-gray-600 text-sm'>Umutwa</label>
                 </div>
-                
+
               </div>
 
-              </fieldset>
+            </fieldset>
 
-              <fieldset className='w-full'>
+            <fieldset className='w-full'>
               <legend className="text-lg font-bold text-gray-600 mb-2">7. Passé politique</legend>
-              <textarea className='w-full h-40 bg-gray-100 text-sm p-4 rounded-md focus:border-primary-color border outline-none' name="" id=""></textarea>
+              <textarea className='w-full h-40 bg-gray-100 text-sm px-4 py-3 rounded-md focus:border-primary-color border outline-none' name="" id=""></textarea>
 
 
             </fieldset>
 
             <fieldset className='w-full'>
               <legend className="text-lg font-bold text-gray-600 mb-2">8. Téléchargements</legend>
-              <label className='w-full flex justify-center py-10 rounded-xl border-dashed border' htmlFor="cardOne">Identité 1</label>
+              <label className='w-full flex justify-center items-center gap-2 text-sm text-gray-600 py-10 rounded-xl border-dashed border' htmlFor="cardOne"><i className="fi fi-rr-add-image translate-y-0.5"></i> Identité 1</label>
               <input className='hidden' id='cardOne' type="file" />
-              <label className='w-full flex justify-center py-10 my-5 rounded-xl border-dashed border' htmlFor="cardTwo">Identité 2</label>
+              <label className='w-full flex justify-center items-center gap-2 text-sm text-gray-600 py-10 my-5 rounded-xl border-dashed border' htmlFor="cardTwo"><i className="fi fi-rr-add-image translate-y-0.5"></i> Identité 2</label>
               <input className='hidden' id='cardTwo' type="file" />
-              <label className='w-full flex justify-center py-10 rounded-xl border-dashed border' htmlFor="photo">Photo passport</label>
+              <label className='w-full flex justify-center items-center gap-2 text-sm text-gray-600 py-10 rounded-xl border-dashed border' htmlFor="photo"><i className="fi fi-rr-add-image translate-y-0.5"></i> Photo passport</label>
               <input className='hidden' id='photo' type="file" />
             </fieldset>
 
-              <div className="flex justify-start">
-                <label className="text-gray-500 text-sm font-bold my-4 flex items-start">
-                  <input className="leading-loose text-pink-600 top-0 translate-y-1" type="checkbox" required />
-                  <small className="ml-2 font-normal text-gray-600 text-left">Accept the
-                    <Link to="#"
-                      className="text-primary-accent px-2">
-                      Terms and Conditions
-                    </Link> of the site and the
-                    <Link to="#"
-                      className="text-primary-accent pl-2">information
-                      data policy.
-                    </Link>
-                  </small>
-                </label>
-              </div>
-              <Button className="md:w-fit w-full bg-primary-color px-10">Devenir Fan</Button>
+            <div className="flex justify-start">
+              <label className="text-gray-500 text-sm font-bold my-4 flex items-start">
+                <input className="leading-loose text-pink-600 top-0 translate-y-1" type="checkbox" required />
+                <small className="ml-2 font-normal text-gray-600 text-left">Accept the
+                  <Link to="#"
+                    className="text-primary-accent px-2">
+                    Terms and Conditions
+                  </Link> of the site and the
+                  <Link to="#"
+                    className="text-primary-accent pl-2">information
+                    data policy.
+                  </Link>
+                </small>
+              </label>
+            </div>
+            <Button className="md:w-fit w-full bg-primary-color px-10">Devenir Membre</Button>
           </form>
         </div>
       </div>
